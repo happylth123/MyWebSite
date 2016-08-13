e9a04b4c5553c78c820d31d07a1213d37a2c8fb1
tenghui <1246623393@qq.com>
2016-08-13 20:12:29 +0800

    ~Underlying architecture

---
+++ MyWebSite/MyWebSite.API/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace MyWebSite.API.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ MyWebSite/MyWebSite.API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MyWebSite.API.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
