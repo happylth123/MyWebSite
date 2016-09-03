f47d4c0ed9c629aafe93f3f1aecd67438736f710
tenghui <457699987@qq.com>
2016-09-03 09:52:11 +0800

    ~some dictionary and list

--- MyWebSite/MyWebSite.Web/Controllers/AccountController.cs
+++ MyWebSite/MyWebSite.Web/Controllers/AccountController.cs
@@ -18,6 +18,26 @@ namespace MyWebSite.Web.Controllers
             var sprints = _sprintService.GetAllSprints();
             _sprintService.CreateSprint();
             _sprintService.UpdateSprint();
+
+
+            #region  Dictionary list
+            Dictionary<string, string> test = new Dictionary<string, string>();
+            test.Add("xingming", "wuyuqing");
+
+            Session["112"] = test;
+            var tessts = Session["112"] as Dictionary<string, string>;
+
+            var xingm = "";
+            test.TryGetValue("xingming", out xingm);
+
+
+            List<string> tesst2 = new List<string>();
+            tesst2.Add("12");
+            tesst2.Add("2");
+            #endregion
+
+
+
             //_sprintService.DeleteSprint("test");
 
             var users = _accountService.GetAllUsers();
