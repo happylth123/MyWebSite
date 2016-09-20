5ab1925518044a76aac246f850f82bb8e61f806d
tenghui <1246623393@qq.com>
2016-09-20 18:04:37 +0800

    ~excel file import

---
+++ MyWebSite/Excel_Csv_Uploader/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Excel_Csv_Uploader.MvcApplication" Language="C#" %>
---
+++ MyWebSite/MyWebSite.Web/App_Start/WebAuthorizeAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace GoBear.TimeTracking.Web.App_Start
+{
+    public class WebAuthorizeAttribute : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            if (base.AuthorizeCore(httpContext))
+            {
+                var cookie = httpContext.Request.Cookies[FormsAuthentication.FormsCookieName];
+                if (cookie != null)
+                {
+                    string cacheKey = cookie.Value;
+                    var authorCache = httpContext.Cache.Get(cacheKey);
+                    if (authorCache == null)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
